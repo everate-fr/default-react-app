feb59f80d8a0c6f7c7f563f572cca8bb7aa76607
lilianQ_Q <lilianpierredamiens@gmail.com>
2023-06-28 22:53:15 +0200

    [INIT] react-template: First commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.tsx',
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'index_bundle.js',
+	},
+	target: 'web',
+	devServer: {
+		port: '3000',
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		open: true,
+		hot: true,
+		liveReload: true,
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.tsx', '.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'public', 'index.html'),
+		}),
+	],
+};
